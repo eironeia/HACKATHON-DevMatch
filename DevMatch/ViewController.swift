e85f5843189d98426454f3bb10b90d073ff63cdc
Alex Eironeia <acuellor.6@gmail.com>
2016-03-12 01:18:02 +0100

    Initial Commit

---
+++ DevMatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevMatch
+//
+//  Created by Alex Cuello ortiz on 12/3/16.
+//  Copyright © 2016 Eironeia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
