bd03ccfec4c7e042a104d33c3b71215e48436acd
cristianpinto <cristianpintoquinto@gmail.com>
2016-03-13 07:52:51 +0100

    preparing segue

--- DevMatch/MenuViewController.swift
+++ DevMatch/MenuViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import Alamofire
 
 class MenuViewController: UIViewController {
+    
+    let id :Int?
     
     @IBOutlet weak var profileButton: UIButton!
     @IBOutlet weak var matchesButton: UIButton!
@@ -24,4 +27,47 @@ class MenuViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func onProfileTapped(sender: AnyObject) {
+        Alamofire.request(.GET, URI+"/user/\(self.id)")
+            .responseJSON {
+                response in
+                switch (response.result){
+                case .Success(let value):
+                    print(value)
+                    
+                    let surname = value["surname"] as! String
+                    
+                    if !surname.isEmpty {
+                        print(surname)
+                        self.surnameField.text = surname
+                    }
+                    
+                    let email = value["email"] as! String
+                    if !email.isEmpty {self.emailField.text = email}
+                    
+                    let phone = value["phone"] as! String
+                    if !phone.isEmpty {self.phoneField.text = phone}
+                    
+                    let age = value["age"] as! String
+                    if !age.isEmpty {self.ageField.text = age}
+                    
+                    /*let city = value["email"] as! String
+                    self.emailField.text = email*/
+                    
+                    
+                case .Failure(let error):
+                    if let data = response.data, let string = String(data: data, encoding: NSUTF8StringEncoding) {
+                        print(string)
+                    }
+                    print(error)
+                }
+        }
+    }
+    @IBAction func onMatchesTapped(sender: AnyObject) {
+    }
+    @IBAction func onEventsTapped(sender: AnyObject) {
+    }
+    @IBAction func onCreateEventTapped(sender: AnyObject) {
+    }
+    
 }
